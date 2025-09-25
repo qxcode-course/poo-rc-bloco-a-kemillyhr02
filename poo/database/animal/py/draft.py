5798965c6fd6d9a5d81a0f8fc3f9066aabe9a333
Kemyhr02 <kemillyholanda11@gmail.com>
2025-09-25 12:53:59 +0000

    summit

--- poo/database/animal/py/draft.py
+++ poo/database/animal/py/draft.py
@@ -1,48 +1,47 @@
 class Animal:
-    def __init__(self, species: str, sound: str):
+    def __init__(self, species: str , sound: str):
         self.species: str = species
         self.sound: str = sound
-        self.age: int= 0
-
-    def __str__(self) -> str :
+        self.age: int= 0 
+    
+    def __str__(self) -> str:
         return f"{self.species}:{self.age}:{self.sound}"
-
-
-    def ageBy(self, increment: int):
+    
+    def ageBy(self, increment: int) -> None:
         self.age+=increment
         if self.age>=4:
             print(f"warning: {self.species} morreu")
             self.age=4
-    
-    def makeSound(self):
-        if self.age==0:
-            print("---")
-        elif self.age==4:
-            print("RIP")
-        else :
-            print(self.sound)
-        
 
-def main():
-    animal = Animal("","")
-    while True:
-        line: str= input()
-        print(f"$" + line)
-        args: list[str] = line.split(" ")
+    def makeSound(self) -> str:
+        if self.age==0:
+            return "---"
+        elif self.age==4:
+            return "RIP"
+        else:
+            return f"{self.sound}"
         
-         
-        if args[0] == "init":
-            species=args[1]
-            sound= args[2]
-            animal= Animal(species, sound)
-        elif args[0]== "show":
-            print(animal)
-        elif args[0]== "end":
+        
+def main():
+    animal=Animal("", "")
+    while True:
+        line= input()
+        print(f"${line}")
+        args= line.split()
+        
+        if args[0]=="end":
             break
-        elif args[0]== "grow":
-            n=int(args[1])
-            animal.ageBy(n)
+        elif args[0]=="init":
+            species=args[1]
+            sound=args[2]
+            animal=Animal(species, sound)
+        elif args[0]=="show":
+            print(animal)
+        elif args[0]=="grow":
+            increment= int(args[1])
+            animal.ageBy(increment)
         elif args[0]=="noise":
-            animal.makeSound()
+            print(animal.makeSound())
+
 
 main()
---
+++ poo/database/carro/py/draft.py
@@ -0,0 +1,60 @@
+class Carro:
+    def __init__(self):
+        self.psg: int=0
+        self.km: int=0
+        self.gas: int=0
+   
+    def __str__(self)->str:
+        return f"pass: {self.psg}, gas: {self.gas}, km: {self.km}"
+    
+    def enter(self):
+        if self.psg>=2:
+            print(f"fail: limite de pessoas atingido")
+        else :
+            self.psg+=1
+    
+    def leave(self):
+        if self.psg==0:
+            print(f"fail: nao ha ninguem no carro")
+        else :
+            self.psg-=1
+    
+    def fuel(self, increment: int):
+        self.gas+=increment
+        if self.gas>=100:
+            self.gas=100
+        
+    def drive(self, distance: int):
+        if self.psg==0:
+            print(f"fail: nao ha ninguem no carro")
+        elif self.gas==0:
+            print(f"fail: tanque vazio")
+        elif self.gas<distance:
+            print(f"fail: tanque vazio apos andar {self.gas} km")
+            self.km+=self.gas
+            self.gas=0
+        else :
+            self.km+=distance
+            self.gas-=distance
+
+def main():
+    carro=Carro()
+    while True:
+        line= input()
+        print(f"${line}")
+        args= line.split()
+        if args[0]=="end":
+            break
+        elif args[0]=="enter":
+            carro.enter()
+        elif args[0]=="show":
+            print(carro)
+        elif args[0]=="leave":
+            carro.leave()
+        elif args[0]=="fuel":
+            increment = int(args[1])
+            carro.fuel(increment)
+        elif args[0]=="drive":
+            increment= int(args[1])
+            carro.drive(increment)
+main()
